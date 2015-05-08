53ab8ab7437456c98cc1db9df208c2b7e5a3702f
Jesse <jmcwilli76@hotmail.com>
2015-05-08 10:44:15 -0700

    More progress

--- ATMPanel.java
+++ ATMPanel.java
@@ -1,5 +1,5 @@
 /*
-  Name:  Jesse McWilliams / Rena Rios / Alejandro
+  Name:  Jesse / Rena / Alejandro
   Prog:  Program 3 – Swing ATM
   Date:  2015-05-05
 */
@@ -14,20 +14,29 @@ import javax.swing.*;
 //  Declare our class
 public class ATMPanel extends JPanel
 {
+  public JLabel lblInstruction, lblDisplay;
+  public JPanel buttonPanel, keypadPanel;
+  private boolean boolPinEntered = false;
+  private String strInputText = "";
+  //  The JLabel allows HTML code.  So we use it to give a nicer looking text message.
+  private String strWelcomeMessage = "<html><center>Welcome<br>To Team #2's<br>Swing ATM program!<br>Written by:<br>Jesse<br>Rena<br>and<br>Alejandro<br><br>Please enter your pin number<br><br>Please use the on screen keypad.</center>";
+  private String strDisplayText = "";
   public ATMPanel()
   {  //  This is our constructor.
     //  Add the instruction area.
-	JLabel lblInstruction = new JLabel("INSTRUCTIONS AREA e.g. Please Enter pin on the numberic keypad");
+	lblInstruction = new JLabel(strWelcomeMessage);
 	lblInstruction.setVerticalTextPosition(JLabel.CENTER);
 	lblInstruction.setHorizontalTextPosition(JLabel.CENTER);
+	lblInstruction.setHorizontalAlignment(JLabel.CENTER);
 	lblInstruction.setBackground(Color.white);
 	lblInstruction.setOpaque(true);
 	lblInstruction.setPreferredSize(new Dimension(350, 200));
 	add(lblInstruction);
 	//  Add the display area.
-	JLabel lblDisplay = new JLabel("Display AREA e.g. Entered text shows up here.");
+	lblDisplay = new JLabel(strDisplayText);
 	lblDisplay.setVerticalTextPosition(JLabel.CENTER);
 	lblDisplay.setHorizontalTextPosition(JLabel.CENTER);
+	lblDisplay.setHorizontalAlignment(JLabel.CENTER);
 	lblDisplay.setBackground(Color.gray);
 	lblDisplay.setOpaque(true);
 	lblDisplay.setPreferredSize(new Dimension(350, 60));
@@ -35,10 +44,10 @@ public class ATMPanel extends JPanel
 	//  Create the listener
     ButtonListener listener = new ButtonListener();  //  Create our listener.  This will get passed to the panel classes.
 	//  Add the Function buttons panel
-	JPanel buttonPanel = new buttPanel(listener);  //  Call the class to create the button panel.
+	buttonPanel = new buttPanel(listener);  //  Call the class to create the button panel.
 	add(buttonPanel);  //  Add the panel to the frame.
 	//  Add the keypad buttons panel
-	JPanel keypadPanel = new keyPanel(listener);  //  Call the class to create the key pad.
+	keypadPanel = new keyPanel(listener);  //  Call the class to create the key pad.
 	add(keypadPanel);  //  Add the panel to the frame.
   }
 
@@ -46,7 +55,8 @@ public class ATMPanel extends JPanel
   //  ****************************************  Testing  *************************************************
 	public static void main(String[] args)
 	{
-	  JFrame frame = new JFrame("Program 3 - ATM");  //  Create the initial frame and set the label.
+	  String strProgramTitle = "Program 3 - ATM";
+	  JFrame frame = new JFrame(strProgramTitle);  //  Create the initial frame and set the label.
 	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //  Set the default close operation.
 	  ATMPanel panel = new ATMPanel();  //  Create an object from our ATMPanel.
 	  frame.getContentPane().add(panel);  //  Add the newly created panel to the Frame.
@@ -58,15 +68,77 @@ public class ATMPanel extends JPanel
   
   //  ****************************************  Testing  *************************************************
   
-  
-  //  This is the button listener where all of the action will be performed.
+  //  **********************************************************************************************
+  //  **********  This is the button listener where all of the action will be performed.  **********
   private class ButtonListener implements ActionListener
   {
      public void actionPerformed(ActionEvent event)
-     {
-	    //  This is where the logic goes.  ******************************************************************
+     {  //  strInputText
+	    String strButtonPressed = ((JButton) event.getSource()).getText().trim();
+		if (strButtonPressed == "Quit")
+		{  //  The user has clicked the Quit button.  Re initialize.
+		  
+		}
+		if (boolPinEntered && strButtonPressed == "1st Withdraw")
+		{  //  Only check the button if the PIN has been entered.
+		  
+		}
+		else if (boolPinEntered && strButtonPressed == "2nd Withdraw")
+		{  //  Only check the button if the PIN has been entered.
+		  
+		}
+		else if (boolPinEntered && strButtonPressed == "3rd Withdraw")
+		{  //  Only check the button if the PIN has been entered.
+		  
+		}
+		else if (boolPinEntered && strButtonPressed == "Deposit")
+		{  //  Only check the button if the PIN has been entered.
+		  
+		}
+		else if (strButtonPressed == "Clear")
+		{  //  Clear the contents of the input string.
+		  strInputText = "";
+		}
+		else if (strButtonPressed == "Enter")
+		{  //  Submit the input string to the next function.
+		  
+		}
+		else
+		{  //  Append to the input string.
+		  strInputText = strInputText + strButtonPressed;
+		}
+	    displayText(strInputText);
      }
+	 public void verifyPIN(Account currentAccount, String PIN)
+	 {  //  This method will verify the PIN number with the account.
+	   int intPIN = Integer.parseInt(PIN);
+	   if (currentAccount.getAccountPIN() == intPIN)
+	   {
+	     boolPinEntered = true;
+		 buttonPanel.enableButtons();
+	   }
+	   else
+	   {
+	     boolPinEntered = false;
+		 buttonPanel.disableButtons();
+	   }
+	 }
+	 public void displayText(String InputText)
+	 {  //  This method will update the text to be displayed.
+	   if (boolPinEntered)
+	   {
+	     lblDisplay.setText(InputText);
+	   }
+	   else
+	   {
+	     lblDisplay.setText(InputText);
+	   }
+	 }
   }
+  //  **********  This is the button listener where all of the action will be performed.  **********
+  //  **********************************************************************************************
+  //  Validate PIN
+  
 
   //  Key Pad Panel
   private class keyPanel extends JPanel
@@ -122,6 +194,7 @@ public class ATMPanel extends JPanel
 	}  //  Button Panel
   private class buttPanel extends JPanel
   {
+    private JButton withdraw1, withdraw2, withdraw3, deposit;
     public buttPanel(ButtonListener listener)
 	{  //  Create the inner panel
 	  JPanel pnlbLeft = new JPanel();
@@ -153,12 +226,36 @@ public class ATMPanel extends JPanel
 	  withdraw3.addActionListener(listener);
 	  deposit.addActionListener(listener);
 	  quit.addActionListener(listener);
+	  //  Disable the buttons by default
+	  //withdraw1.setEnabled(false);
+	  //withdraw2.setEnabled(false);
+	  //withdraw3.setEnabled(false);
+	  //deposit.setEnabled(false);
 	  //  Set default size of panel
 	  pnlbLeft.revalidate();
 	  //pnlbLeft.setSize(new Dimension(xCord,5 * yCord));
 	  //  Add this panel to the calling panel
 	  add(pnlbLeft);
-
+	}
+	public void enableButtons()
+	{  //  Allow all the buttons to be disabled.
+	  withdraw1.setEnabled(true);
+	  withdraw2.setEnabled(true);
+	  withdraw3.setEnabled(true);
+	  deposit.setEnabled(true);
+	}
+	public void disableButtons()
+	{  //  Allow all the buttons to be enabled.
+	  withdraw1.setEnabled(false);
+	  withdraw2.setEnabled(false);
+	  withdraw3.setEnabled(false);
+	  deposit.setEnabled(false);
+	}
+	public void disableWithdrawl()
+	{  //  Disable all of the Withdraw buttons.
+	  withdraw1.setEnabled(false);
+	  withdraw2.setEnabled(false);
+	  withdraw3.setEnabled(false);
 	}
   }
 }
